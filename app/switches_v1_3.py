b09c2d7950ec23ef14a851ab98bd9d584fcf9e39
cloudysunny14 <cloudysunny14@gmail.com>
2013-09-16 16:46:05 +0900

    Correspond to Ryu 3.0, Fixed correct implementation for handle lldp send loop

--- app/switches_v1_3.py
+++ app/switches_v1_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2012 cloudysunny14.
+# Copyright 2013 cloudysunny14.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,15 +21,16 @@ import time
 from ryu.base.app_manager import RyuApp
 from ryu.controller import ofp_event
 from ryu.controller.handler import set_ev_cls
-from ryu.controller.handler import CONFIG_DISPATCHER
-from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.ofproto.ofproto_v1_2 import OFPG_ANY
 from ryu.ofproto.ofproto_v1_3 import OFP_VERSION
 from ryu.lib.mac import DONTCARE_STR
-from ryu.lib.port_no import port_no_to_str
-from ryu.lib.dpid import dpid_to_str
 from ryu.ofproto.ether import ETH_TYPE_LLDP
 from ryu.topology.switches import LLDPPacket
+from ryu.topology.switches import Port, PortState
+from ryu.topology.switches import PortDataState
+from ryu.topology.switches import Link, LinkState
+from ryu.topology.switches import Switch
 from ryu.topology import event
 from ryu.lib import hub
 
@@ -41,12 +42,14 @@ class L2Switch(RyuApp):
                event.EventPortModify,
                event.EventLinkAdd, event.EventLinkDelete]
 
-    DEFAULT_TTL = 120
     OFP_VERSIONS = [OFP_VERSION]
+    
+    DEFAULT_TTL = 120  # unused. ignored.
     LLDP_PACKET_LEN = len(LLDPPacket.lldp_packet(0, 0, DONTCARE_STR, 0))
+
     LLDP_SEND_GUARD = .05
     LLDP_SEND_PERIOD_PER_PORT = .9
-    TIMEOUT_CHECK_PERIOD = 30.
+    TIMEOUT_CHECK_PERIOD = 5.
     LINK_TIMEOUT = TIMEOUT_CHECK_PERIOD * 2
     LINK_LLDP_DROP = 5
 
@@ -74,7 +77,6 @@ class L2Switch(RyuApp):
     def lldp_loop(self):
         while self.is_active:
             self.lldp_event.clear()
-
             now = time.time()
             timeout = None
             ports_now = []
@@ -91,7 +93,6 @@ class L2Switch(RyuApp):
 
                 timeout = expire - now
                 break
-
             for port in ports_now:
                 self.send_lldp_packet(port.dpid, 
                                       port.port_no,
@@ -165,6 +166,9 @@ class L2Switch(RyuApp):
         assert dp.id not in self.dps
 
         self.dps[dp.id] = dp
+        self.port_state[dp.id] = PortState()
+        for port in dp.ports.values():
+            self.port_state[dp.id].add(port.port_no, port)
         
     def _link_down(self, port):
         try:
@@ -242,7 +246,6 @@ class L2Switch(RyuApp):
     def switch_features_handler(self, ev):
         """Handle switch features reply to install table miss flow entries."""
         datapath = ev.msg.datapath
-        self._register(datapath)
         self.install_table_miss(datapath, 0)
 
     def create_match(self, parser, fields):
@@ -275,34 +278,18 @@ class L2Switch(RyuApp):
                                           [output_port], lldp_data)
         datapath.send_msg(packet_out)
 
-    @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER)
-    def port_desc_stats_reply_handler(self, ev):
-        msg = ev.msg
-        datapath = msg.datapath
-        self.show_port_desc(msg.body, datapath)
-    
-    def show_port_desc(self, body, datapath):
-        self.port_state[datapath.id] = PortState()
-        for p in body:
-            self.port_state[datapath.id].add(p.port_no, p)
-            self.send_lldp_packet(datapath.id, p.port_no,
-                                  p.hw_addr)
-            port = self._get_port(datapath.id, p.port_no)
-            if port and not port.is_reserved():
-                self._port_added(port)
-                self.lldp_event.set()
-
-    def send_port_desc_stats_request(self, datapath):
-        ofp_parser = datapath.ofproto_parser
-        req = ofp_parser.OFPPortDescStatsRequest(datapath, 0)
-        datapath.send_msg(req)
-    
     @set_ev_cls(ofp_event.EventOFPBarrierReply, MAIN_DISPATCHER)
     def barrier_reply_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        self.send_port_desc_stats_request(datapath)
-
+        self._register(datapath)
+        switch = self._get_switch(datapath.id)
+        self.send_event_to_observers(event.EventSwitchEnter(switch))
+        for port in switch.ports:
+            if not port.is_reserved():
+                self._port_added(port)
+        self.lldp_event.set()
+        
     def create_flow_mod(self, datapath, priority,
                         table_id, match, instructions):
         """Create OFP flow mod message."""
@@ -385,292 +372,4 @@ class L2Switch(RyuApp):
                 else:
                     self.ports.move_front(dst)
                     self.lldp_event.set()
-
-class PortState(dict):
-    # dict: int port_no -> OFPPort port
-    # OFPPort is defined in ryu.ofproto.ofproto_v1_X_parser
-    def __init__(self):
-        super(PortState, self).__init__()
-
-    def add(self, port_no, port):
-        self[port_no] = port
-
-    def remove(self, port_no):
-        del self[port_no]
-
-    def modify(self, port_no, port):
-        self[port_no] = port
-
-class Port(object):
-    # This is data class passed by EventPortXXX
-    def __init__(self, dpid, ofproto, ofpport):
-        super(Port, self).__init__()
-
-        self.dpid = dpid
-        self._ofproto = ofproto
-        self._config = ofpport.config
-        self._state = ofpport.state
-
-        self.port_no = ofpport.port_no
-        self.hw_addr = ofpport.hw_addr
-        self.name = ofpport.name
-
-    def is_reserved(self):
-        return self.port_no > self._ofproto.OFPP_MAX
-
-    def is_down(self):
-        return (self._state & self._ofproto.OFPPS_LINK_DOWN) > 0 \
-            or (self._config & self._ofproto.OFPPC_PORT_DOWN) > 0
-
-    def is_live(self):
-        # NOTE: OF1.2 has OFPPS_LIVE state
-        #       return (self._state & self._ofproto.OFPPS_LIVE) > 0
-        return not self.is_down()
-
-    def to_dict(self):
-        return {'dpid': dpid_to_str(self.dpid),
-                'port_no': port_no_to_str(self.port_no),
-                'hw_addr': self.hw_addr,
-                'name': self.name.rstrip('\0')}
-
-    # for Switch.del_port()
-    def __eq__(self, other):
-        return self.dpid == other.dpid and self.port_no == other.port_no
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self):
-        return hash((self.dpid, self.port_no))
-
-    def __str__(self):
-        LIVE_MSG = {False: 'DOWN', True: 'LIVE'}
-        return 'Port<dpid=%s, port_no=%s, %s>' % \
-            (self.dpid, self.port_no, LIVE_MSG[self.is_live()])
-
-class PortData(object):
-    def __init__(self, is_down, lldp_data):
-        super(PortData, self).__init__()
-        self.is_down = is_down
-        self.lldp_data = lldp_data
-        self.timestamp = None
-        self.sent = 0
-
-    def lldp_sent(self):
-        self.timestamp = time.time()
-        self.sent += 1
-
-    def lldp_received(self):
-        self.sent = 0
-
-    def lldp_dropped(self):
-        return self.sent
-
-    def clear_timestamp(self):
-        self.timestamp = None
-
-    def set_down(self, is_down):
-        self.is_down = is_down
-
-    def __str__(self):
-        return 'PortData<live=%s, timestamp=%s, sent=%d>' \
-            % (not self.is_down, self.timestamp, self.sent)
-
-class PortDataState(dict):
-    # dict: Port class -> PortData class
-    # slimed down version of OrderedDict as python 2.6 doesn't support it.
-    _PREV = 0
-    _NEXT = 1
-    _KEY = 2
-
-    def __init__(self):
-        super(PortDataState, self).__init__()
-        self._root = root = []          # sentinel node
-        root[:] = [root, root, None]    # [_PREV, _NEXT, _KEY]
-                                        # doubly linked list
-        self._map = {}
-
-    def _remove_key(self, key):
-        link_prev, link_next, key = self._map.pop(key)
-        link_prev[self._NEXT] = link_next
-        link_next[self._PREV] = link_prev
-
-    def _append_key(self, key):
-        root = self._root
-        last = root[self._PREV]
-        last[self._NEXT] = root[self._PREV] = self._map[key] = [last, root,
-                                                                key]
-
-    def _prepend_key(self, key):
-        root = self._root
-        first = root[self._NEXT]
-        first[self._PREV] = root[self._NEXT] = self._map[key] = [root, first,
-                                                                 key]
-
-    def _move_last_key(self, key):
-        self._remove_key(key)
-        self._append_key(key)
-
-    def _move_front_key(self, key):
-        self._remove_key(key)
-        self._prepend_key(key)
-
-    def add_port(self, port, lldp_data):
-        if port not in self:
-            self._prepend_key(port)
-            self[port] = PortData(port.is_down(), lldp_data)
-        else:
-            self[port].is_down = port.is_down()
-
-    def lldp_sent(self, port):
-        port_data = self[port]
-        port_data.lldp_sent()
-        self._move_last_key(port)
-        return port_data
-
-    def lldp_received(self, port):
-        self[port].lldp_received()
-
-    def move_front(self, port):
-        port_data = self.get(port, None)
-        if port_data is not None:
-            port_data.clear_timestamp()
-            self._move_front_key(port)
-
-    def set_down(self, port):
-        is_down = port.is_down()
-        port_data = self[port]
-        port_data.set_down(is_down)
-        port_data.clear_timestamp()
-        if not is_down:
-            self._move_front_key(port)
-        return is_down
-
-    def get_port(self, port):
-        return self[port]
-
-    def del_port(self, port):
-        del self[port]
-        self._remove_key(port)
-
-    def __iter__(self):
-        root = self._root
-        curr = root[self._NEXT]
-        while curr is not root:
-            yield curr[self._KEY]
-            curr = curr[self._NEXT]
-
-    def clear(self):
-        for node in self._map.itervalues():
-            del node[:]
-        root = self._root
-        root[:] = [root, root, None]
-        self._map.clear()
-        dict.clear(self)
-
-    def items(self):
-        'od.items() -> list of (key, value) pairs in od'
-        return [(key, self[key]) for key in self]
-
-    def iteritems(self):
-        'od.iteritems -> an iterator over the (key, value) pairs in od'
-        for k in self:
-            yield (k, self[k])
-
-class LinkState(dict):
-    # dict: Link class -> timestamp
-    def __init__(self):
-        super(LinkState, self).__init__()
-        self._map = {}
-
-    def get_peer(self, src):
-        return self._map.get(src, None)
-
-    def update_link(self, src, dst):
-        link = Link(src, dst)
-
-        self[link] = time.time()
-        self._map[src] = dst
-
-        # return if the reverse link is also up or not
-        rev_link = Link(dst, src)
-        return rev_link in self
-
-    def link_down(self, link):
-        del self[link]
-        del self._map[link.src]
-
-    def rev_link_set_timestamp(self, rev_link, timestamp):
-        # rev_link may or may not in LinkSet
-        if rev_link in self:
-            self[rev_link] = timestamp
-
-    def port_deleted(self, src):
-        dst = self.get_peer(src)
-        if dst is None:
-            raise KeyError()
-
-        link = Link(src, dst)
-        rev_link = Link(dst, src)
-        del self[link]
-        del self._map[src]
-        # reverse link might not exist
-        self.pop(rev_link, None)
-        rev_link_dst = self._map.pop(dst, None)
-
-        return dst, rev_link_dst
-
-class Switch(object):
-    # This is data class passed by EventSwitchXXX
-    def __init__(self, dp):
-        super(Switch, self).__init__()
-
-        self.dp = dp
-        self.ports = []
-
-    def add_port(self, ofpport):
-        port = Port(self.dp.id, self.dp.ofproto, ofpport)
-        if not port.is_reserved():
-            self.ports.append(port)
-
-    def del_port(self, ofpport):
-        self.ports.remove(Port(ofpport))
-
-    def to_dict(self):
-        d = {'dpid': dpid_to_str(self.dp.id),
-             'ports': [port.to_dict() for port in self.ports]}
-        return d
-
-    def __str__(self):
-        msg = 'Switch<dpid=%s, ' % self.dp.id
-        for port in self.ports:
-            msg += str(port) + ' '
-
-        msg += '>'
-        return msg
-
-class Link(object):
-    # This is data class passed by EventLinkXXX
-    def __init__(self, src, dst):
-        super(Link, self).__init__()
-        self.src = src
-        self.dst = dst
-
-    def to_dict(self):
-        d = {'src': self.src.to_dict(),
-             'dst': self.dst.to_dict()}
-        return d
-
-    # this type is used for key value of LinkState
-    def __eq__(self, other):
-        return self.src == other.src and self.dst == other.dst
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self):
-        return hash((self.src, self.dst))
-
-    def __str__(self):
-        return 'Link: %s to %s' % (self.src, self.dst)
 
